8d9edcf22460abeb454658dd1447d7581efc155e
Pantoja <mateuspantoja9@gmail.com>
2023-10-30 20:20:55 -0400

    inicio de javascript

---
+++ js/aula1.js
@@ -0,0 +1,39 @@
+// function fn2() {
+//     setTimeout(() => {
+//         console.log('oi');
+//     })
+// }
+
+// function fn1() {
+//     fn2();
+// }
+
+// function main() {
+//     fn1();
+// }
+
+// main();
+// console.log('Tchau mundo');
+
+// var nome = "Mateus";
+// console.log(nome);
+
+// nome = 22;+
+// console.log(nome);
+
+
+// console.log('10' + '2');
+
+
+const valor1 = 'V1';
+// const valor3 = [];
+
+function main() {
+    valor1 = 'V2';
+    console.log(valor1);
+
+}
+
+
+main();
+console.log(valor1);
