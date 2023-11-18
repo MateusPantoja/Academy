d0fbca8c248a5c741451d840295c7a1ee3053284
Pantoja <mateuspantoja9@gmail.com>
2023-11-17 20:15:54 -0400

    Bug do NG Module

--- angular/academy-project/src/app/app.component.ts
+++ angular/academy-project/src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { AppModule } from './app.module';
+
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, MeuComponenteComponent],
+  standalone: false,
+  imports: [CommonModule, RouterOutlet, MeuComponenteComponent, HeroesComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'academy-project';
   helloWord = {
---
+++ angular/academy-project/src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Hero } from '../Hero';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  hero: Hero = {
+    id: 0,
+    name: 'Birulei',
+    idade: 0
+  }
+}
