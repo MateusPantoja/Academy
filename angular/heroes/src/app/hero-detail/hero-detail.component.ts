490a1a268405b8bbb14a924dbd0b2cd86bcd7c01
Mateus Pantoja <mspantoja@hub-uea.org>
2023-11-29 19:25:39 -0500

    aula de routes

--- angular/heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular/heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,10 +12,27 @@ import { Hero } from '../Hero';
 })
 export class HeroDetailComponent implements OnInit {
 
-  
-  @Input() hero?: Hero //input indica que hero ta herdando de outro componente
+
+  // @Input() hero?: Hero //input indica que hero ta herdando de outro componente
+  hero: Hero | undefined
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {  }
 
   ngOnInit(): void {
-    
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back()
   }
 }
--- angular/heroes/src/app/hero.service.ts
+++ angular/heroes/src/app/hero.service.ts
@@ -4,24 +4,19 @@ import { Hero } from './Hero';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
-// Criou o serviço para retorno dos valores das simulaçoes
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-
-  constructor(private messageService: MessageService) {}
-
-
-  // getHeroes(): Hero[] {
-  //   return HEROES
-  // }
-
-  //
-  getHeroesMock(): Observable<Hero[]> {
+  constructor(private messageService: MessageService){}
+  getHeroesMock():Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add("Hero Service: fetched heroes");
     return heroes
   }
-
+  getHero(id: number):Observable<Hero> {
+    const hero = HEROES.find(heroValue => heroValue.id === id)!;
+    this.messageService.add(`HeroService: Fetched hero id=${id}`)
+    return of(hero)
+  }
 }
