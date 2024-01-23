f360a20ee451cef20b82c9f33c60cdcd1d00b99c
Pantoja <mateuspantoja9@gmail.com>
2024-01-22 20:31:17 -0400

    utima aula

--- webAcademy/src/pages/home/home-component/home-component.component.ts
+++ webAcademy/src/pages/home/home-component/home-component.component.ts
@@ -1,18 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from 'src/service/home-service/home.service';
-
 @Component({
   selector: 'app-home-component',
   templateUrl: './home-component.component.html',
   styleUrls: ['./home-component.component.css']
 })
 export class HomeComponentComponent implements OnInit {
+  alunos = [];
+  constructor(private service : HomeService) {
 
-  constructor(
-    private homeService: HomeService) { }
+  }
 
-  ngOnInit() {
+  ngOnInit(): void {
+
+    this.service.getAlunos().subscribe((data) => {
+      this.alunos = data
+    })
+  }
+
+  insertAlunos(){
+    console.log('AQUIIIIIIIIIII');
+
+    const data = {
+      nome : 'Lucas',
+      idade: 25
+    }
+
+    return this.service.insertAlunos(data).subscribe((data) => {
+      return  console.log(data)
+    })
   }
 
 
+
 }
+
+
--- webAcademy/src/service/home-service/home.service.ts
+++ webAcademy/src/service/home-service/home.service.ts
@@ -11,8 +11,13 @@ export class HomeService {
 constructor() { }
 
   getAlunos() : Observable<any> {
+    let headers = {
+      'Content-type': 'application/json',
+      'Access-Control-Allow-Origin': 'http://localhost:3000'
+    }
+
     return new Observable((observer) => {
-      axios.get(this.api)
+      axios.get(this.api+"/alunos")
         .then((response) => {
           observer.next(response.data);
           observer.complete()
@@ -22,6 +27,17 @@ constructor() { }
         })
     })
   }
+  insertAlunos(data:any):  Observable<any> {
+    return new Observable((observer) => {
+      axios.post(this.api+"/alunos", data)
+        .then((response) => {
 
+        })
+        .catch((error) =>{
+          observer.error(error)
+        })
+    })
+
+  }
 }
 
